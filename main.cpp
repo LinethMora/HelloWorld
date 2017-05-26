965276c6bada00c0eb36bc4ab11c8de780f1721b
lineth <linethep22@gmail.com>
2017-05-26 08:25:20 -0600

    primer

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: lineth
+ *
+ * Created on 26 de mayo de 2017, 07:49 AM
+ */
+
+#include <cstdlib>
+#include <malloc.h>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Hola Mundo");
+
+    return 0;
+}
+
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=pruebagithub
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/pruebagithub
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=pruebagithub.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/pruebagithub.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=pruebagithub
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/pruebagithub
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=pruebagithub.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/pruebagithub.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
